6b721a3eb0585cf21b2aecad83e85869986ed7b3
markoarula <74591376+marko-arula@users.noreply.github.com>
2023-02-02 12:27:58 +0200

    Update yl3.ps1

--- yl3.ps1
+++ yl3.ps1
@@ -1,1 +1,43 @@
+function Translate {
+    param(
+    	[string] $inputstring
+     	 )
+  	$Translate = @{
+    
+     	[char]'ä' = "a"
+     	[char]'õ' = "o"
+     	[char]'ü' = "u"
+     	[char]'ö' = "o"
+    
+      }
+     	
+      $outputstring=""
+     	
+      foreach ($character in $inputCharacter = $inputstring.ToCharArray())
+    	{
+     	if ($Translit[$character] -cne $Null){
+     	$outputString += $Translit[$character]
+     	} else {
+        	$outputString += $character
+    	}
+     	 }
+        Write-Output $outputstring
+        }
 
+$eesnimi = Read-Host "Palun sisestada kasutaja eesnimi"
+$perenimi = Read-Host "Palun sisestada kasutaja perenimi"
+$parool = Read-Host "Palun sisestada kasutaja parool" | ConvertTo-SecureString -AsPlainText -Force
+$taisnimi = "$eesnimi.$perenimi"
+$taisnimi = Translit($taisnimi)
+$adnimi = $taisnimi + "@sv-kool.local"
+$kirjeldus = "$eesnimi $perenimi windowsi konto"
+
+if (Get-ADUser -F {SamAccountName -eq $taisnimi}) 
+    { 
+    Write-host "Kasutaja nimega $taisnimi on juba olemas - nii et seda ei saa lisada" 
+    }
+    else 
+    {
+    New-ADUser -Name $taisnimi -DisplayName $taisnimi -GivenName $taisnimi -Title $taisnimi -UserPrincipalName $adnimi -AccountPassword (ConvertTo-SecureString $parool -AsPlainText -force) -Enabled $true 
+    Write-host "Kasutaja $taisnimi on nüüd edukalt lisatud süsteemi"
+    }
